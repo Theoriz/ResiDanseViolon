403ad9005a9152460dd55ac0571b926d0d36ce4e
AlexBourgeois <abourgeois27380@gmail.com>
2019-04-15 13:52:49 +0200

    Data scripts

---
+++ Assets/Scripts/DataAnalyzer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataAnalyzer : MonoBehaviour
+{
+    [Header("Hot hand")]
+    public float XValue;
+    public float YValue;
+    public float ZValue;
+
+    public float XDerivated;
+    public float YDerivated;
+    public float ZDerivated;
+
+    private float _xOldValue;
+    private float _yOldValue;
+    private float _zOldValue;
+
+    public float refreshRate;
+    private float _lastTimeRefreshed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _xOldValue = XValue;
+        _yOldValue = YValue;
+        _zOldValue = ZValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time - _lastTimeRefreshed >= (1/refreshRate))
+        {
+            XDerivated = XValue - _xOldValue;
+            _xOldValue = XValue;
+
+            YDerivated = YValue - _yOldValue;
+            _yOldValue = YValue;
+
+            ZDerivated = ZValue - _zOldValue;
+            _zOldValue = ZValue;
+
+            _lastTimeRefreshed = Time.time;
+        }
+    }
+}
---
+++ Assets/Scripts/DataAnalyzerControllable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataAnalyzerControllable : Controllable
+{
+    [OSCProperty] public float XValue;
+    [OSCProperty] public float YValue;
+    [OSCProperty] public float ZValue;
+
+    [OSCProperty] public float XDerivated;
+    [OSCProperty] public float YDerivated;
+    [OSCProperty] public float ZDerivated;
+}
