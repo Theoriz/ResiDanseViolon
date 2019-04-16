96b5042c67b3bf767e6d892a43be9ac03766ce37
Benoit Arbelot <benoit.arbelot@gmail.com>
2019-04-16 11:24:35 +0200

    Added Tableau4.

---
+++ Assets/BookTemplate/Tableau4/Scripts/Tableau4Light.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tableau4Light : MonoBehaviour
+{
+	public KAPPS.KAPPS kapps;
+	public float floor;
+	public float speed;
+	public float radius;
+	public float intensity;
+	public Color color;
+
+	private float age = 0;
+
+	// Start is called before the first frame update
+	void Start() {
+		age = 0;
+
+		Light light = GetComponent<Light>();
+		light.intensity = intensity;
+		light.range = radius;
+		light.color = color;
+
+		KAPPS.KAPPSSource source = GetComponent<KAPPS.KAPPSSource>();
+		source.particleSystemList = new List<KAPPS.KAPPS>();
+		source.particleSystemList.Add(kapps);
+
+		source.radius = radius;
+		source.intensity = intensity;
+
+		//Enable the source that is disabled in the prefab to not initialize itself when instantiated
+		source.enabled = true;
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+		age += Time.deltaTime;
+
+		transform.Translate(0, -Time.deltaTime * speed, 0);
+
+		if (transform.localPosition.y < floor) {
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/BookTemplate/Tableau4/Scripts/Tableau4LightsController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tableau4LightsController : MonoBehaviour
+{
+	public KAPPS.KAPPS kapps;
+	public GameObject lightPrefab;
+
+	public float floor;
+
+	public Color color;
+
+	public float minRadius;
+	public float maxRadius;
+
+	public float minIntensity;
+	public float maxIntensity;
+
+	public float minFrequency;
+	public float maxFrequency;
+
+	public float minSpeed;
+	public float maxSpeed;
+
+	private float timer;
+	private float currentFrequency;
+
+
+	private void Start() {
+		timer = 0;
+		currentFrequency = Random.Range(minFrequency, maxFrequency);
+	}
+
+	// Update is called once per frame
+	void Update() {
+		timer += Time.deltaTime;
+
+		if (timer > 1.0f / currentFrequency) {
+
+			CreateLight();
+			timer = 0;
+			currentFrequency = Random.Range(minFrequency, maxFrequency);
+
+		}
+	}
+
+	void CreateLight() {
+
+		GameObject newLight = Instantiate(lightPrefab, transform);
+
+		Tableau4Light lightParam = newLight.GetComponent<Tableau4Light>();
+
+		lightParam.kapps = kapps;
+		lightParam.speed = Random.Range(minSpeed, maxSpeed);
+		lightParam.floor = floor;
+		lightParam.radius = Random.Range(minRadius, maxRadius);
+		lightParam.intensity = Random.Range(minIntensity, maxIntensity);
+		lightParam.color = color;
+	}
+}
