d558c16611894867b3a4d67d2d729dc374aaace8
AlexBourgeois <abourgeois27380@gmail.com>
2019-04-16 14:25:54 +0200

    Audio reactiv stream

---
+++ Assets/BookTemplate/Stream/AudioLightSensitive.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioLightSensitive : MonoBehaviour
+{
+    public float intensity;
+    public float scale;
+
+    public Lasp.FilterType filterType;
+
+    public Light light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        light.intensity = Lasp.MasterInput.GetPeakLevel(filterType) * scale;
+    }
+}
